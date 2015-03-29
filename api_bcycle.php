714daec9d325c763cd4342b1af6f40aae2f92891
Diego <diego.hernandez@ceinf.cl>
2015-03-29 18:09:36 -0300

    Modificado el Codigo
    Se cambio al codigo mandado y se agrrego que se saltara los SSL

--- api_bcycle.php
+++ api_bcycle.php
@@ -1,33 +1,24 @@
 <?php
-	$url = "https://portal.bikesharedev.com/1/subscriptiontypes/";
-	//  Initiate curl
-	$ch = curl_init();
-	// Disable SSL verification
-	curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "GET"); 
-	
-	curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
-	// Will return the response, if false it print the response
-	
-	
-	curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-	
-	curl_setopt($ch, CURLOPT_HTTPHEADER, array('BCycle-Program-Token : fkIaYFsjmWzSgRdDLvWbXobb049ZVOvus36nUToZ77X0eIl1uhXpBxKKT1pu1vrk'));
-	// Set the url
-	curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1); 
-	curl_setopt($ch, CURLOPT_URL,$url);
-	// Execute
-	$result = curl_exec($ch);
-	// Closing
-	curl_close($ch);
-	
-	if ($output === FALSE) {
-         echo "cURL Error: " . curl_error($ch);
-     }
-	
-	var_dump($result);
 
-	// Will dump a beauty json :3
-	$data = json_decode($result, true);
-	
-	//echo $data["error"];
-?>
+$url = "https://portal.bikesharedev.com/1/subscriptiontypes";
+
+$ch = curl_init();
+$opciones = array(
+    CURLOPT_HTTPHEADER => array('BCycle-Program-Token : fkIaYFsjmWzSgRdDLvWbXobb049ZVOvus36nUToZ77X0eIl1uhXpBxKKT1pu1vrk'),
+    CURLOPT_URL => 'https://portal.bikesharedev.com/1/subscriptiontypes',
+    CURLOPT_RETURNTRANSFER => 1,
+    CURLOPT_VERBOSE => 1,
+    CURLINFO_HEADER_OUT => 1,
+    CURLOPT_SSL_VERIFYPEER => false
+);
+curl_setopt_array($ch, $opciones);
+
+$result = curl_exec($ch);
+if ($result === FALSE) {
+    echo "cURL Error: " . curl_error($ch);
+} else {
+    curl_close($ch);
+
+    $data = json_decode($result, true);
+    echo $data["stackTrace"];
+}
